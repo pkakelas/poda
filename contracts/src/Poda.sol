27add4aa44a3e32d2ce9a03256e502ba1aa5956b
Dimitris Lamprinos <pkakelas@gmail.com>
2025-06-23 18:57:05 +0300

    Remove openzeppelin dependency from contracts

--- contracts/src/Poda.sol
+++ contracts/src/Poda.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import { MerkleProof } from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import { MerkleProof } from "./lib/MerkleProof.sol";
 
 contract Poda {
     struct Commitment {
